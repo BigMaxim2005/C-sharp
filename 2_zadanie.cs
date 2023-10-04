9b8bffa38da175a7c12d4a35df69ef2d54f578b2
BigMaxim2005 <34681573+BigMaxim2005@users.noreply.github.com>
2023-10-04 21:13:41 +0300

    Add files via upload

---
+++ 2_zadanie.cs
@@ -0,0 +1,23 @@
+﻿int n;
+while(true)
+{
+    Console.Write("Ведите целое число:");
+    string num = Console.ReadLine();
+    if (int.TryParse(num, out n))
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            Console.Write(i + " ");
+        }
+
+        for (int i = n - 1; i >= 1; i--)
+        {
+            Console.Write(i + " ");
+        }
+        Console.WriteLine();
+        break;
+    }
+    Console.WriteLine("Вы ввели не целое число");
+
+} 
+
